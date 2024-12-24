3f608978714750359f4b69f864b4a4fc0d4a425a
perugi <dominik.perusko@gmail.com>
2024-12-24 14:12:18 +0100

    Add Template Syntax examples

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const msg = '<span style="color: red">Hello World</span>'
+const id = 'test'
+const isButtonDisabled = ''
+const seen = true
+
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper',
+  style: 'background-color: red',
+}
+
+const eventName = 'focus'
+
+const eventHandler = () => {
+  console.log(`You triggered the ${eventName} event`)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p v-bind="objectOfAttrs">Message: {{ msg.toUpperCase() }}</p>
+  <p :id="id + '-test'">message</p>
+  <p>Message: <span v-html="msg"></span></p>
+  <button :disabled="isButtonDisabled">Button</button>
+  <p v-if="seen">Now you see me</p>
+  <button @[eventName]="eventHandler">What to do with me</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+#test {
+  color: blue;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#test-test {
+  color: gold;
 }
 </style>
